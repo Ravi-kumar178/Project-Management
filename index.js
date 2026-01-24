be3d3f442d480f53a17c1da00587abea5439cde2
Ravi Kumar <bhardwajravi2025@gmail.com>
2026-01-25 00:39:37 +0530

    added nodemon and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hy Ravi here");
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+let username = process.env.name;
+console.log(username);
+console.log("ravi");
+
+console.log("Got it");
